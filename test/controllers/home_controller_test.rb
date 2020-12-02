3a3d4e2e98fdcc580bf853ba9c5e5913c5e5f637
Louisa Isaeva$ <louisaisaeva2002@gmail.com>
2020-12-02 20:19:33 +0300

    hi

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,6 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get input" do
-    get home_input_url
-    assert_response :success
-  end
-
-  test "should get output" do
-    get home_output_url
-    assert_response :success
-  end
+ 
 
 end
